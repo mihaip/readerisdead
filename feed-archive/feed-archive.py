5d2fd6457d43966b9962ca59c262ba01f12884c3
Mihai Parparita <mihai@persistent.info>
2013-03-31 17:56:10 -0700

    OPML option for specifying feed URLs

--- feed-archive/feed-archive.py
+++ feed-archive/feed-archive.py
@@ -27,8 +27,20 @@ def main():
   log.init()
   parser = argparse.ArgumentParser(
       description='Fetch archived feed data from Google Reader')
-  parser.add_argument('feed_urls', metavar='feed_url', nargs='+',
-                      help='Feed URL to fetch archived data for')
+  # Which feeds to fetch data for
+  feed_group = parser.add_mutually_exclusive_group()
+  feed_group.add_argument('feed_urls', metavar='feed_url', nargs='*',
+                          default=[],
+                          help='Feed URL to fetch archived data for')
+  feed_group.add_argument('--opml_file', default='',
+                          help='OPML file listing feed URLs to fetch archived '
+                          'data for')
+
+  # Output options
+  parser.add_argument('--output_directory', default='./',
+                      help='Directory where to place feed archive data. Use '
+                            '"-" to output archive data to stdout.')
+  # Fetching options
   parser.add_argument('--chunk_size', type=int, default=1000,
                       help='Number of items to request per Google Reader API '
                            'call (higher is more efficient)')
@@ -43,16 +55,18 @@ def main():
                       help='Timestamp (in seconds since the epoch) of the '
                            'newest item that should be returned (0 for no '
                            'timestamp restriction)')
-  parser.add_argument('--output_directory', default='./',
-                      help='Directory where to place feed archive data. Use '
-                            '"-" to output archive data to stdout.')
+
   args = parser.parse_args()
-  _BASE_PARAMETERS['n'] = args.chunk_size
-  if args.oldest_item_timestamp_sec:
-    _BASE_PARAMETERS['ot'] = args.oldest_item_timestamp_sec
-  if args.newest_item_timestamp_sec:
-    _BASE_PARAMETERS['nt'] = args.newest_item_timestamp_sec
-  for feed_url in args.feed_urls:
+  if args.opml_file:
+    feed_urls = extract_feed_urls_from_opml_file(args.opml_file)
+  else:
+    feed_urls = args.feed_urls
+  init_base_parameters(args)
+
+  logging.info('Fetching archived data for %d feed%s',
+      len(feed_urls), len(feed_urls) == 1 and '' or 's')
+
+  for feed_url in feed_urls:
     if args.output_directory != '-':
       output_path = get_output_path(args.output_directory, feed_url)
     else:
@@ -62,8 +76,28 @@ def main():
 # params
 # - validate URLs
 # - retry attempts
-# - OPML file (local or remote) to use for feed URLs
+# - OPML file URL
 # - output naming scheme
+
+def extract_feed_urls_from_opml_file(opml_file_path):
+  tree = ET.parse(opml_file_path)
+  feed_urls = []
+  seen_feed_urls = set()
+  for outline in tree.iter(tag='outline'):
+    if 'xmlUrl' in outline.attrib:
+      feed_url = outline.attrib['xmlUrl']
+      if feed_url in seen_feed_urls:
+        continue
+      feed_urls.append(feed_url)
+      seen_feed_urls.add(feed_url)
+  return feed_urls
+
+def init_base_parameters(args):
+  _BASE_PARAMETERS['n'] = args.chunk_size
+  if args.oldest_item_timestamp_sec:
+    _BASE_PARAMETERS['ot'] = args.oldest_item_timestamp_sec
+  if args.newest_item_timestamp_sec:
+    _BASE_PARAMETERS['nt'] = args.newest_item_timestamp_sec
 
 def get_output_path(base_path, feed_url):
   file_name = feed_url
@@ -74,6 +108,8 @@ def get_output_path(base_path, feed_url):
   for c in [os.sep, '/', ':', '?']:
     file_name = file_name.replace(c, '-')
   return os.path.join(base_path, file_name)
+
+
 
 def fetch_feed(feed_url, max_items, output_path):
   continuation_token = None
@@ -86,20 +122,20 @@ def fetch_feed(feed_url, max_items, output_path):
     reader_url = (
       'http://www.google.com/reader/public/atom/hifi/feed/%s?%s' %
       (urllib.quote(feed_url), urllib.urlencode(parameters)))
-    logging.debug("Fetching %s", reader_url)
+    logging.debug('Fetching %s', reader_url)
     request = urllib2.Request(reader_url)
     response = urllib2.urlopen(request)
     response_tree = ET.parse(response)
     response_root = response_tree.getroot()
     entries = response_root.findall('{%s}entry' % _ATOM_NS)
-    oldest_message = ""
+    oldest_message = ''
     if entries:
       last_crawl_timestamp_msec = \
           entries[-1].attrib['{%s}crawl-timestamp-msec' % _READER_NS]
       last_crawl_timestamp = datetime.datetime.utcfromtimestamp(
           float(last_crawl_timestamp_msec)/1000)
-      oldest_message = " (oldest is from %s)" % last_crawl_timestamp
-    logging.info("Loaded %d items%s", len(entries), oldest_message)
+      oldest_message = ' (oldest is from %s)' % last_crawl_timestamp
+    logging.info('Loaded %d items%s', len(entries), oldest_message)
     if combined_feed:
       combined_feed.extend(entries)
     else:
@@ -121,10 +157,10 @@ def fetch_feed(feed_url, max_items, output_path):
 
   if output_path:
     output_file = open(output_path, 'w')
-    logging.info("Writing %d items to %s" % (total_entries, output_path))
+    logging.info('Writing %d items to %s' % (total_entries, output_path))
   else:
     output_file = sys.stdout
-    logging.info("Writing %d items to stdout" % total_entries)
+    logging.info('Writing %d items to stdout' % total_entries)
   combined_feed_tree.write(
       output_file,
       xml_declaration=True,
@@ -132,5 +168,5 @@ def fetch_feed(feed_url, max_items, output_path):
   if output_path:
     output_file.close()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
