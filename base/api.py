c0423e7747a48b860ea8100756932ac6b9ef9de7
Mihai Parparita <mihai@persistent.info>
2013-06-22 12:05:43 -0700

    Include friends in the list of streams to fetch.

--- base/api.py
+++ base/api.py
@@ -39,6 +39,41 @@ class Api(object):
       ))
     return result
 
+  def fetch_friends(self):
+    friends_json = self._fetch_json('GET', 'friend/list')
+    result = []
+    for friend_json in friends_json['friends']:
+      flags = friend_json.get("flags", 0)
+      types = friend_json.get("types", [])
+      websites=[
+        Website(w['title'], w['url']) for w in friend_json.get('websites', [])]
+      result.append(Friend(
+        stream_id=friend_json.get('stream'),
+
+        user_ids=friend_json.get('userIds', []),
+        profile_ids=friend_json.get('profileIds', []),
+        contact_id=friend_json['contactId'],
+        group_ids=friend_json.get('groupIds', []),
+
+        display_name=friend_json['displayName'],
+        given_name=friend_json['givenName'],
+        occupation=friend_json.get('occupation'),
+        websites=websites,
+        location=friend_json.get('location'),
+        photo_url=friend_json.get('photoUrl'),
+        email_addresses=friend_json.get('emailAddresses', []),
+
+        is_current_user=flags & 1 << 0,
+        is_hidden=flags & 1 << 1,
+        is_new=flags & 1 << 2,
+        uses_reader=flags & 1 << 3,
+        is_follower=0 in types,
+        is_following=1 in types,
+        is_invite=2 in types,
+        is_contact=3 in types,
+      ))
+    return result
+
   def _fetch_json(self, http_method, api_path):
     request = urllib2.Request(
         'https://www.google.com/reader/api/0/%s?output=json' % api_path,
@@ -59,5 +94,25 @@ Subscription = collections.namedtuple(
   'Subscription',
   ['stream_id', 'title', 'sort_id', 'first_item_usec', 'html_url',
   'insert_stream_ids'])
+
+Friend = collections.namedtuple(
+  'Friend',
+  [
+    # Shared items stream
+    'stream_id',
+
+    # Ids
+    'user_ids', 'profile_ids', 'contact_id', 'group_ids',
+
+    # Profile data
+    'display_name', 'given_name', 'occupation', 'websites', 'location',
+    'photo_url', 'email_addresses',
+
+    # Flags
+    'is_current_user', 'is_hidden', 'is_new', 'uses_reader',
+    'is_follower', 'is_following', 'is_invite', 'is_contact',
+  ])
+
+Website = collections.namedtuple('Website', ['title', 'url'])
 
 UserInfo = collections.namedtuple('UserInfo', ['user_id', 'email'])
